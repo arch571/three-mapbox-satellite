cadc6ab709567389d058203ff5b2711d0357593c
arch571 <archsun571@gmail.com>
2024-01-24 12:01:29 +0530

    version 0.3.4 changes

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # three-mapbox-satellite
 
-
-**three-mapbox-satellite** is a complete refactor and simplified version of [three-geo](https://github.com/w3reality/three-geo). It renders the satellite image of a lat-lon as a threejs Mesh. Currently it exports a single class and it is the responsibility of the consumer to import and build it using their own builders (webpack, rollup etc). 
+**three-mapbox-satellite** is a complete refactor and simplified version of [three-geo](https://github.com/w3reality/three-geo). It renders the satellite image of a lat-lon as a threejs Mesh. Currently it exports a single class and it is the responsibility of the consumer to import and build it using their own builders (webpack, rollup etc).
 
 ## Why
 
@@ -15,29 +14,25 @@ $ yarn add three-mapbox-satellite
 
 ## Usage
 
-The API is predominently similar to [three-geo](https://github.com/w3reality/three-geo), with some small changes. 
-The terrain/satellite image is returned as a threejs group with the terrain meshes as children. 
-
+The API is predominently similar to [three-geo](https://github.com/w3reality/three-geo), with some small changes.
+The terrain/satellite image is returned as a threejs group with the terrain meshes as children.
 
 Here is an example of how to build a geographic terrain located at GPS coordinates (46.5763, 7.9904) in a 5 km radius circle. The terrain's satellite zoom resolution is set to 12. (The highest zoom value supported is 17.)
 
 ```js
-import ThreeMapboxSatellite from 'three-mapbox-satellite';
+import ThreeMapboxSatellite from "three-mapbox-satellite";
 
 // setup your threejs scene and camera prior to this
- 
-const sat_tiles = new ThreeMapboxSatellite(46.5763, 
-                                          7.9904, 
-                                          12, 
-                                          5.0, 
-                                          { clip: false, 
-                                            render_box_size: THREE_BOX_SIZE,
-                                            api_token: MAPBOX_TOKEN 
-                                          })
+
+const sat_tiles = new ThreeMapboxSatellite(46.5763, 7.9904, 12, 5.0, {
+  clip: false,
+  render_box_size: THREE_BOX_SIZE,
+  api_token: MAPBOX_TOKEN,
+});
 
 const group = await sat_tiles.renderSatellite();
-if(group) scene.add(group)
-const projection = sat_tiles.getProjection();   //for geo to xy and vice-versa . see below for explanation
+if (group) scene.add(group);
+const projection = sat_tiles.getProjection(); //for geo to xy and vice-versa . see below for explanation
 ```
 
 ## API
@@ -61,12 +56,12 @@ const projection = sat_tiles.getProjection();   //for geo to xy and vice-versa .
   - `opts.render_box_size`= 1.0 **number** The side length of the square that fits the terrain in WebGL space. (same as unitsSide in three-geo)
 
   - `opts.clip`= false **boolean** By default it renders all the mapbox tiles within the radius, which extends beyond the render box size. Set clip to true to add clippingPanes. For it to work, the WebGLRenderer needs to have localClipping set to true
-  
-- `async renderSatellite(progressCallback=()=>{})` 
+
+- `async renderSatellite(progressCallback=()=>{})`
+
   - `progressCallback` **optional function** A callback method which receives the completion percentage as argument (a number between 0 and 1). Typically used by calling UI to display progressbars
 
   Return a **THREE.Group** object that represents a 3D surface of the terrain.
-
 
   The group object contains an **Array\<THREE.Mesh\>** as `.children`. Each mesh corresponds to a partial geometry of the terrain textured with satellite images.
 
@@ -78,19 +73,21 @@ const projection = sat_tiles.getProjection();   //for geo to xy and vice-versa .
 
   - `xy2LonLat(xy)` is a function that maps WebGL coordinates `[x, y]` to geo coordinates `[lon, lat]`.
 
-  - `bbox` is an object `{ nw, se }` that represents the computed bounding box of the terrain. `nw` is an array with first element is the north latitude and second is the west longitude value. `se` is an array with first element is the south latitude value and second is the east longitude value. 
+  - `bbox` is an object `{ nw, se }` that represents the computed bounding box of the terrain. `nw` is an array with first element is the north latitude and second is the west longitude value. `se` is an array with first element is the south latitude value and second is the east longitude value.
 
   - `units_per_meter` is the length in WebGL-space per meter.
 
 ## Change Log
- * Version 0.2.1 - Refactored and removed Promise.all to avoid simultaneous queries to mapbox 
- * Version 0.2.2 - Added promise pool for queries to mapbox rather than Promise.all
- * Version 0.3.0 - Added progress callback for renderSatellite and changed internal variables from snake_case to camelCase
- * Version 0.3.2 - Added mesh dimensions to userData
- * Version 0.3.3 - Changed MAX_TILES to 36 from 32
+
+- Version 0.2.1 - Refactored and removed Promise.all to avoid simultaneous queries to mapbox
+- Version 0.2.2 - Added promise pool for queries to mapbox rather than Promise.all
+- Version 0.3.0 - Added progress callback for renderSatellite and changed internal variables from snake_case to camelCase
+- Version 0.3.2 - Added mesh dimensions to userData
+- Version 0.3.3 - Changed MAX_TILES to 36 from 32
+- Version 0.3.4 - Colorspace fix for texture
 
 ## Credits
-* [three-geo](https://github.com/w3reality/three-geo)
-* [Mapbox API](https://docs.mapbox.com/api/overview/)
-* This [gist](https://gist.github.com/ChaseIngebritson/22803b340664becdc08b03683e9f935d) from ChaseIngebritson
 
+- [three-geo](https://github.com/w3reality/three-geo)
+- [Mapbox API](https://docs.mapbox.com/api/overview/)
+- This [gist](https://gist.github.com/ChaseIngebritson/22803b340664becdc08b03683e9f935d) from ChaseIngebritson
--- geo_helper.js
+++ geo_helper.js
@@ -1,35 +1,49 @@
 import SphericalMercator from "@mapbox/sphericalmercator";
-import { TextureLoader } from "three";
+import { SRGBColorSpace, TextureLoader } from "three";
 
-const degree2Radian = v=>v*Math.PI/180;
-const radian2Degree = v=>v*180/Math.PI;
+const degree2Radian = (v) => (v * Math.PI) / 180;
+const radian2Degree = (v) => (v * 180) / Math.PI;
 
 // https://wiki.openstreetmap.org/wiki/Slippy_map_tilenames#ECMAScript_.28JavaScript.2FActionScript.2C_etc..29
-const lon2TileX = (lon,zoom) => Math.floor((lon+180)/360*Math.pow(2,zoom));
-const lat2TileY = (lat,zoom) => Math.floor((1-Math.log(Math.tan(lat*Math.PI/180) + 
-                                  1/Math.cos(lat*Math.PI/180))/Math.PI)/2 *Math.pow(2,zoom));
-
+const lon2TileX = (lon, zoom) =>
+  Math.floor(((lon + 180) / 360) * Math.pow(2, zoom));
+const lat2TileY = (lat, zoom) =>
+  Math.floor(
+    ((1 -
+      Math.log(
+        Math.tan((lat * Math.PI) / 180) + 1 / Math.cos((lat * Math.PI) / 180)
+      ) /
+        Math.PI) /
+      2) *
+      Math.pow(2, zoom)
+  );
 
 //https://movable-type.co.uk/scripts/latlong.html - bearing section
-//turf-destination library   
+//turf-destination library
 const getGeoDestination = (lat, lon, d, bearing) => {
   const lt1 = degree2Radian(lat);
   const ln1 = degree2Radian(lon);
   const rbearing = degree2Radian(bearing);
   const R = 6373; //in km
 
-  const lt2 = Math.asin( Math.sin(lt1)*Math.cos(d/R) +
-                                  Math.cos(lt1)*Math.sin(d/R)*Math.cos(rbearing) );
-  const ln2 = ln1 + Math.atan2(Math.sin(rbearing)*Math.sin(d/R)*Math.cos(lt1),
-                                       Math.cos(d/R)-Math.sin(lt1)*Math.sin(lt2));
+  const lt2 = Math.asin(
+    Math.sin(lt1) * Math.cos(d / R) +
+      Math.cos(lt1) * Math.sin(d / R) * Math.cos(rbearing)
+  );
+  const ln2 =
+    ln1 +
+    Math.atan2(
+      Math.sin(rbearing) * Math.sin(d / R) * Math.cos(lt1),
+      Math.cos(d / R) - Math.sin(lt1) * Math.sin(lt2)
+    );
   return [radian2Degree(lt2), radian2Degree(ln2)];
-}
+};
 
 const getBBoxFromOrigin = (olat, olon, radiusInKm) => {
   const nw = getGeoDestination(olat, olon, radiusInKm, -45);
   const se = getGeoDestination(olat, olon, radiusInKm, 135);
-  return { nw, se }
-}
+  return { nw, se };
+};
 
 const getBBoxTilePos = (bbox, zoom) => {
   const x1 = lon2TileX(bbox.nw[1], zoom);
@@ -40,23 +54,23 @@ const getBBoxTilePos = (bbox, zoom) => {
 
   let m = [];
 
-  for(let y=y1, row=0; y <= y2; y++, row++) {
-    for(let x=x1, col=0; x <= x2; x++, col++) {
+  for (let y = y1, row = 0; y <= y2; y++, row++) {
+    for (let x = x1, col = 0; x <= x2; x++, col++) {
       m.push(`${zoom}/${x}/${y}`);
     }
   }
   return m;
-}
+};
 
 const getPixels = async (url) => {
-  return new Promise ((resolve, reject) => {
+  return new Promise((resolve, reject) => {
     const img = new Image();
-    img.crossOrigin = 'Anonymous';
+    img.crossOrigin = "Anonymous";
 
     img.onload = () => {
-      const canvas = document.createElement('canvas');
-      const context = canvas.getContext('2d');
-  
+      const canvas = document.createElement("canvas");
+      const context = canvas.getContext("2d");
+
       canvas.width = img.width;
       canvas.height = img.width;
 
@@ -64,16 +78,15 @@ const getPixels = async (url) => {
 
       const imgData = context.getImageData(0, 0, img.width, img.height);
       return resolve(imgData.data);
-    }
-    img.onerror = (e) =>{
-      console.log(`Error loading mapbox ${url}`)
+    };
+    img.onerror = (e) => {
+      console.log(`Error loading mapbox ${url}`);
       return reject(e);
-    }
+    };
 
     img.src = url;
   });
-} //end getPixels
-     
+}; //end getPixels
 
 const createTexture = async (zoom, x, y, apiToken) => {
   // const url = `https://api.mapbox.com/v4/mapbox.satellite/${zoom}/${x}/${y}@2x.png?access_token=${this.apiToken}`;
@@ -81,99 +94,117 @@ const createTexture = async (zoom, x, y, apiToken) => {
 
   const textureLoader = new TextureLoader();
   const texture = await textureLoader.loadAsync(url);
+  //set colorspace as it seems brighter without it
+  texture.colorSpace = SRGBColorSpace;
   return texture;
-}
+};
 
 const getElevationsFromTile = async (zoom, x, y, apiToken) => {
   const url = `https://api.mapbox.com/v4/mapbox.terrain-rgb/${zoom}/${x}/${y}@2x.pngraw?access_token=${apiToken}`;
   const pixels = await getPixels(url);
-  return getElevationsFromPixels(pixels)
-}
+  return getElevationsFromPixels(pixels);
+};
 
-const getElevationsFromPixels = (pixels)=>{
+const getElevationsFromPixels = (pixels) => {
   //https://docs.mapbox.com/help/troubleshooting/access-elevation-data/
-  const _rgbToElevation = (r, g, b)=>-10000 + ((r * 256 * 256 + g * 256 + b) * 0.1)
-  
+  const _rgbToElevation = (r, g, b) =>
+    -10000 + (r * 256 * 256 + g * 256 + b) * 0.1;
+
   const elevations = [];
-  for(let i=0;i<pixels.length; i+=4) {
+  for (let i = 0; i < pixels.length; i += 4) {
     const r = pixels[i + 0];
     const g = pixels[i + 1];
     const b = pixels[i + 2];
     elevations.push(_rgbToElevation(r, g, b));
   }
   return elevations;
-}
+};
 
-const convertToXYZ = (tilePosStr, tileElevations, { elevationDim, unitsPerMeter, lonLat2XY }) => {
-  const mercator = new SphericalMercator({ size: elevationDim })
-  let [tileZoom, tileX, tileY] = tilePosStr.split('/').map(i=>parseInt(i));
+const convertToXYZ = (
+  tilePosStr,
+  tileElevations,
+  { elevationDim, unitsPerMeter, lonLat2XY }
+) => {
+  const mercator = new SphericalMercator({ size: elevationDim });
+  let [tileZoom, tileX, tileY] = tilePosStr.split("/").map((i) => parseInt(i));
   const tileData = [];
-  for(let row=0;row<elevationDim;row++) {
-    for(let col=0;col<elevationDim;col++) {
-      const pixelLonLat = mercator.ll([
-        tileX*elevationDim+col,
-        tileY*elevationDim+row
-      ], tileZoom)
-      tileData.push(...lonLat2XY(pixelLonLat), tileElevations[row*elevationDim+col]*unitsPerMeter)
+  for (let row = 0; row < elevationDim; row++) {
+    for (let col = 0; col < elevationDim; col++) {
+      const pixelLonLat = mercator.ll(
+        [tileX * elevationDim + col, tileY * elevationDim + row],
+        tileZoom
+      );
+      tileData.push(
+        ...lonLat2XY(pixelLonLat),
+        tileElevations[row * elevationDim + col] * unitsPerMeter
+      );
     }
   }
   return tileData;
-}
+};
 
-const getSouthTileIndex = function(tilePosStr, tilePosArray) {
-  let [zoom, x, y] = tilePosStr.split('/').map(i=>parseInt(i));
-  const southTileStr = `${zoom}/${x}/${y+1}`;
-  return tilePosArray.indexOf(southTileStr); 
-}
+const getSouthTileIndex = function (tilePosStr, tilePosArray) {
+  let [zoom, x, y] = tilePosStr.split("/").map((i) => parseInt(i));
+  const southTileStr = `${zoom}/${x}/${y + 1}`;
+  return tilePosArray.indexOf(southTileStr);
+};
 
-const getEastTileIndex = function(tilePosStr, tilePosArray) {
-  let [zoom, x, y] = tilePosStr.split('/').map(i=>parseInt(i));
-  const eastTileStr = `${zoom}/${x+1}/${y}`;
-  return tilePosArray.indexOf(eastTileStr); 
-}
+const getEastTileIndex = function (tilePosStr, tilePosArray) {
+  let [zoom, x, y] = tilePosStr.split("/").map((i) => parseInt(i));
+  const eastTileStr = `${zoom}/${x + 1}/${y}`;
+  return tilePosArray.indexOf(eastTileStr);
+};
 
-const getSouthEastTileIndex = function(tilePosStr, tilePosArray) {
-  let [zoom, x, y] = tilePosStr.split('/').map(i=>parseInt(i));
-  const southEastTileStr = `${zoom}/${x+1}/${y+1}`;
-  return tilePosArray.indexOf(southEastTileStr); 
-}
+const getSouthEastTileIndex = function (tilePosStr, tilePosArray) {
+  let [zoom, x, y] = tilePosStr.split("/").map((i) => parseInt(i));
+  const southEastTileStr = `${zoom}/${x + 1}/${y + 1}`;
+  return tilePosArray.indexOf(southEastTileStr);
+};
 
-const addSouthRow = function(baseTile, southTile, elevationDim) {
-  baseTile.push(...southTile.slice(0, elevationDim*3))
-}
+const addSouthRow = function (baseTile, southTile, elevationDim) {
+  baseTile.push(...southTile.slice(0, elevationDim * 3));
+};
 
-const addEastColumn = function(baseTile, eastTile, elevationDim) {
-  for(let i=0;i<elevationDim;i++) {
+const addEastColumn = function (baseTile, eastTile, elevationDim) {
+  for (let i = 0; i < elevationDim; i++) {
     const si = i * elevationDim * 3; //source index
     //add extra vertex of prev rows
     const ti = i * ((elevationDim + 1) * 3) + elevationDim * 3;
-    baseTile.splice(ti, 0, eastTile[si], eastTile[si+1], eastTile[si+2])
+    baseTile.splice(ti, 0, eastTile[si], eastTile[si + 1], eastTile[si + 2]);
   }
-}
+};
 
-const addSouthEastPixel = function(baseTile, southEastTile) {
-  baseTile.push(...southEastTile.slice(0,3));
-}
+const addSouthEastPixel = function (baseTile, southEastTile) {
+  baseTile.push(...southEastTile.slice(0, 3));
+};
 
-const addSeams = tileArray => {
-  const tilePosArray = tileArray.map(t=>t.tilePosStr);
-  for( let tileObj of tileArray) {
+const addSeams = (tileArray) => {
+  const tilePosArray = tileArray.map((t) => t.tilePosStr);
+  for (let tileObj of tileArray) {
     let posIndex = getSouthTileIndex(tileObj.tilePosStr, tilePosArray);
-    if(posIndex >= 0) {
-      addSouthRow(tileObj.tileData, tileArray[posIndex].tileData, tileObj.elevationDim);
+    if (posIndex >= 0) {
+      addSouthRow(
+        tileObj.tileData,
+        tileArray[posIndex].tileData,
+        tileObj.elevationDim
+      );
       tileObj.ySegments++;
     }
     posIndex = getEastTileIndex(tileObj.tilePosStr, tilePosArray);
-    if(posIndex >= 0) {
-      addEastColumn(tileObj.tileData, tileArray[posIndex].tileData, tileObj.elevationDim);
+    if (posIndex >= 0) {
+      addEastColumn(
+        tileObj.tileData,
+        tileArray[posIndex].tileData,
+        tileObj.elevationDim
+      );
       tileObj.xSegments++;
     }
     posIndex = getSouthEastTileIndex(tileObj.tilePosStr, tilePosArray);
-    if(posIndex >= 0) {
+    if (posIndex >= 0) {
       addSouthEastPixel(tileObj.tileData, tileArray[posIndex].tileData);
     }
   }
-}
+};
 
 export {
   lon2TileX,
@@ -184,5 +215,5 @@ export {
   getElevationsFromTile,
   convertToXYZ,
   createTexture,
-  addSeams
-}
+  addSeams,
+};
